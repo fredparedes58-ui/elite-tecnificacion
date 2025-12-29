f2bfa8686f71aa61db004a03fab4fd347a553424
fredparedes58 <fredparedes58@gmail.com>
2025-12-29 14:56:12 +0000

    Changes before Firebase Studio auto-run

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:myapp/main.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
